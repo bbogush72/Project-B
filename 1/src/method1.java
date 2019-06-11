51acc6c46806a09b2979575f4138e5b176f3204f
Ozlem <Ozlem@Lilys>
2019-06-11 00:17:36 -0500

    .

---
+++ 1/src/method1.java
@@ -0,0 +1,44 @@
+public class method1 {
+	public static void main(String[] args) {
+
+		Person personA = new Person();
+		personA.name = "John";
+		personA.age = 37;
+		personA.speak();
+		
+
+		Person personB = new Person();
+		personB.name = "Sarah";
+		personB.age = 35;
+		
+		
+		
+
+		personB.speak();
+		
+		System.out.println(personA.name);
+		System.out.println(personA.age);
+		System.out.println("-------------------------------------");
+
+		System.out.println(personB.name);
+		System.out.println(personB.age);
+		
+		Person person1 = new Person();
+
+		person1.name = "Joe";
+		person1.age = 25;
+		
+	
+	personA.sayHello();
+	personB.sayHello2();
+	
+
+	// classes contain 
+	//1-data 
+	//2-subroutines (methods)
+
+
+	}
+}
+
+
---
+++ 1/src/method_sub.java
@@ -0,0 +1,31 @@
+
+class Person {
+	String name;
+	int age;
+
+	void speak2() {
+		System.out.println("My name is " + name);
+
+	}
+
+
+	public void sayHello2() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello there, this is void no return type method");
+	}
+	
+	
+	void speak() {
+		for(int i=0; i<2; i++) {
+		System.out.println("My name is "+ name + " and I am " + age + " years old ");
+		}
+	}
+	
+	void sayHello() {
+		
+		System.out.println("Hello there!" + name + " How are you, today? " + name + " is testing this code and busy it seems");
+	}
+
+
+
+}
